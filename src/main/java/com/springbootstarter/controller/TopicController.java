ca14022884f2c068e382b619500b8cbcad5d0a46
Amit <amit.sharma045@gmail.com>
2017-02-18 15:14:51 +0530

    Adding udpate and delete service
    Incorporated with the review comments
    
    Signed-off-by: Amit <amit.sharma045@gmail.com>

--- src/main/java/com/springbootstarter/controller/TopicController.java
+++ src/main/java/com/springbootstarter/controller/TopicController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springbootstarter.Topic;
@@ -24,11 +25,17 @@ public class TopicController {
 	public Topic getTopic(@PathVariable("id") String id ){
 		return topicService.getTopic(id);
 	}
-	@RequestMapping(method = org.springframework.web.bind.annotation.RequestMethod.POST, value ="/topics")
+	@RequestMapping(method = RequestMethod.POST, value ="/topics")
 	public void addTopic(@RequestBody Topic topic){
 		topicService.addTopic(topic);
 	}
-	
-	
+	@RequestMapping(method = RequestMethod.PUT, value ="/topics/{id}")
+	public void updateTopic(@RequestBody Topic topic,@PathVariable String id){
+		topicService.updateTopic(topic,id);
+	}
+	@RequestMapping(method = RequestMethod.DELETE, value ="/topics/{id}")
+	public void deleteTopic(@RequestBody Topic topic,@PathVariable String id){
+		topicService.deleteTopic(topic,id);
+	}
 	
 }
--- src/main/java/com/springbootstarter/service/TopicService.java
+++ src/main/java/com/springbootstarter/service/TopicService.java
@@ -11,20 +11,31 @@ import com.springbootstarter.Topic;
 @Service
 public class TopicService {
 	List<Topic> topics= new ArrayList<>( Arrays.asList(
-	new Topic("C","C","C is a Language"),
-	new Topic("C++","C++","C++ is a Language"),
-	new Topic("Java","Java","Java is a Language"),
-	new Topic("Python","Python","Python is a Language")));
-	
+			new Topic("C","C","C is a Language"),
+			new Topic("C++","C++","C++ is a Language"),
+			new Topic("Java","Java","Java is a Language"),
+			new Topic("Python","Python","Python is a Language")));
+
 	public List<Topic> getAllTopics(){
 		return topics;
 	}
-	
+
 	public Topic getTopic(String id){
 		return topics.stream().filter(t-> t.getId().equals(id)).findFirst().get();
 	}
-	
+
 	public void addTopic(Topic topic){
 		topics.add(topic);
+	}
+	
+	public void updateTopic(Topic topic, String id){
+		Topic updateTopic=topics.stream().filter(t-> t.getId().equals(id)).findFirst().get();
+		updateTopic.setDesription(topic.getDesription());
+		updateTopic.setName(topic.getName());
+	}
+
+	public void deleteTopic(Topic topic, String id) {
+		topics.remove(topics.stream().filter(t-> t.getId().equals(id)).findFirst().get());
+		
 	}
 }
